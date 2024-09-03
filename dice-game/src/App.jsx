cf715161e89791ec8ae40653d20e1e77be4c8aea
semih turan <semituran@gmail.com>
2024-09-03 17:19:19 +0300

    add dice game files

--- dice-game/src/App.jsx
+++ dice-game/src/App.jsx
@@ -1,35 +1,163 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Component } from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //State update
+  const [inputValue, setInputValue] = useState("Player 1");
+  //input update
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <PlayerInput
+            onInputChange={handleInputChange}
+            playerName={inputValue}
+          />
+          <RoolDice inputValue={inputValue} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
+}
+export default App;
+
+//show dice picture
+export class Dice extends Component {
+  render() {
+    let cls = `./images/${this.props.img}`;
+    return (
+      <div>
+        <img src={cls} />
+      </div>
+    );
+  }
 }
 
-export default App
+//rolls the dice and calculates the results
+export class RoolDice extends Component {
+  static defaultProps = {
+    sides: [
+      "dice1.png",
+      "dice2.png",
+      "dice3.png",
+      "dice4.png",
+      "dice5.png",
+      "dice6.png",
+    ],
+  };
+  state = {
+    dice1: "dice1.png",
+    dice2: "dice2.png",
+    rolling: false,
+    winner: "",
+  };
+
+  // Roll the dice and start animation
+  roll = () => {
+    this.setState({ rolling: true, winner: "" });
+
+    // Zarları her 100ms'de bir değiştir
+    const rollingInterval = setInterval(() => {
+      const newDice1 =
+        this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+      const newDice2 =
+        this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
+      this.setState({ dice1: newDice1, dice2: newDice2 });
+    }, 100);
+    setTimeout(() => {
+      clearInterval(rollingInterval);
+      this.setState({ rolling: false }, this.determineWinner);
+    }, 3000);
+  };
+
+  //Determines the winner and assigns it to the state
+  determineWinner = () => {
+    const { dice1, dice2 } = this.state;
+    const dice1Value = parseInt(dice1.replace("dice", "").replace(".png", ""));
+    const dice2Value = parseInt(dice2.replace("dice", "").replace(".png", ""));
+
+    let winner;
+    if (dice1Value > dice2Value) {
+      winner = (
+        <>
+          <h2>{this.props.inputValue} Wins!</h2>
+          <p>Player 2 Loses</p>
+        </>
+      );
+    } else if (dice1Value < dice2Value) {
+      winner = (
+        <>
+          <h2>Player 2 Wins!</h2>
+          <p>{this.props.inputValue} Loses </p>
+        </>
+      );
+    } else {
+      winner = (
+        <h1>
+          Draw! <i className="fas fa-handshake"></i>
+        </h1>
+      );
+    }
+
+    this.setState({ winner });
+  };
+  render() {
+    return (
+      <div>
+        <div className="dice">
+          <Dice img={this.state.dice1} rolling={this.state.rolling} />
+          <Dice img={this.state.dice2} rolling={this.state.rolling} />
+        </div>
+        <button
+          className="btn"
+          onClick={this.roll}
+          disabled={this.state.rolling}
+        >
+          {this.state.rolling ? (
+            "Rolling..."
+          ) : (
+            <i className="fa-solid fa-shuffle"></i>
+          )}{" "}
+        </button>
+
+        {this.state.winner && <div className="winner">{this.state.winner}</div>}
+      </div>
+    );
+  }
+}
+
+//Used to get name from user
+export class PlayerInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: props.playerName || "Player 1",
+    };
+  }
+
+  handleChange = (event) => {
+    const newValue = event.target.value;
+    this.setState({ inputValue: newValue });
+    this.props.onInputChange(newValue);
+  };
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          placeholder="İsminizi giriniz..."
+        />
+        <div className="player matemasie-regular">
+          <p> {this.state.inputValue}</p>
+          <p>Player 2</p>
+        </div>
+      </div>
+    );
+  }
+}
